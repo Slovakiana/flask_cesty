56809361140d7d3aadc0e74c518ad1285312e459
Slovakiana <radovan.kudela@icloud.com>
2025-06-27 10:31:29 +0200

    Update init_py

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'tajny_kluc'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/app.db'
+app.config['SECRET_KEY'] = 'tvojesilnetajomstvopridajsem'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_cesty.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+csrf = CSRFProtect(app)
 
+# Import routes aby boli zaregistrované
 from app import routes, models
 
+# Vytvorenie databázových tabuliek automaticky pri štarte na Render
 with app.app_context():
+    print("✅ Spúšťam db.create_all() pre inicializáciu tabuliek...")
     db.create_all()
