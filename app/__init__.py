41e7bfea660a9e9eea4d529d9605af65e4415589
Slovakiana <radovan.kudela@icloud.com>
2025-06-27 10:39:01 +0200

    Update Init znovu

--- app/__init__.py
+++ app/__init__.py
@@ -5,17 +5,19 @@ from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-
-# Bezpecne nacitanie SECRET_KEY z prostredia
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev_secret')
-
-# Databaza - Render odporucane pouzit SQLite alebo PostgreSQL
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///cesty.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+# AUTOMATIC DATABASE INIT
+with app.app_context():
+    from app import models
+    db.create_all()
+
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'   # ✅ toto opravi Unauthorized
+login_manager.login_view = 'login'
 csrf = CSRFProtect(app)
 
 from app import routes, models
